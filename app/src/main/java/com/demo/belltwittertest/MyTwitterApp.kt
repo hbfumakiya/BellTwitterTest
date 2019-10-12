c21dc7358212c62617206ab81034847aaa1a7974
Hardik Fumakiya <hardik.fumakiya@tracktik.com>
2019-10-12 18:05:51 -0400

    first commit with twitter account setup

---
+++ app/src/main/java/com/demo/belltwittertest/MyTwitterApp.kt
@@ -0,0 +1,28 @@
+package com.demo.belltwittertest
+
+import android.app.Application
+import android.util.Log
+import com.demo.belltwittertest.utils.Const
+import com.twitter.sdk.android.core.DefaultLogger
+import com.twitter.sdk.android.core.Twitter
+import com.twitter.sdk.android.core.TwitterAuthConfig
+import com.twitter.sdk.android.core.TwitterConfig
+
+/**
+ * Created by Hardik on 2019-10-12.
+ */
+class MyTwitterApp :Application() {
+    override fun onCreate() {
+        super.onCreate()
+       val twitterConfig= configureTwitterInstance()
+
+        Twitter.initialize(twitterConfig)
+    }
+
+    private fun configureTwitterInstance()= TwitterConfig.Builder(this)
+                                                .logger(DefaultLogger(Log.DEBUG))
+                                                .twitterAuthConfig(TwitterAuthConfig(Const.TWITTER_CONSUMER_API_KEY,Const.TWITTER_CONSUMER_API_SECRET_KEY))
+                                                .debug(true)
+                                                .build()
+
+}
---
+++ app/src/main/java/com/demo/belltwittertest/utils/Const.kt
@@ -0,0 +1,15 @@
+package com.demo.belltwittertest.utils
+
+/**
+ * Created by Hardik on 2019-10-12.
+ */
+object Const {
+    const val TWITTER_CONSUMER_API_KEY= "Wl5BJqzjHh30yPkH1G1N5hcJd"
+    const val TWITTER_CONSUMER_API_SECRET_KEY= "MzJ1rdM3YZoSEUeiLxsmhHzZWYiCRAVyRb47ZkaNBshgPCvPuN"
+
+    const val TWITTER_ACCESS_TOKEN_KEY= "1097662795-kewmBipAh5BFPwQYZlUJNa4axCqg4OHrNzuxEpN"
+    const val TWITTER_ACCESS_TOKEN_SECRET_KEY= "ZA1eQsjcAuBpEhUX8MOGpwbXFPzlxpDrUJVRv7rWObe3B"
+
+
+
+}
